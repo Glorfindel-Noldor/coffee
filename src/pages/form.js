87e0b53017c75e6a2ad851cd7f9526292569f285
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-04-05 17:08:36 -0500

    functioning form

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 function Form(){
+    const { api, coffee, setCoffee } = useOutletContext();
+    const [stateName, setStateName] = useState('')
+    const [stateDescription, setStateDescription] = useState('')
 
 
+    const InputChange= (e)=>{
+        const {name, value} = e.target
+        if (name === 'name' && value.length < 11) {
+            setStateName(value);
+        } else if (name === 'description') {
+            setStateDescription(value);
+        }
+    }
 
+    const FormSubmit= (e)=>{
+        e.preventDefault();
+        if(stateDescription.length < 2 && stateName.length < 2){
+            alert(`type some more stuff`)
+            return null
+        }
+        const newCoffee = {
+            "name": stateName,
+            "description": stateDescription
+        }
+        const post = {
+            method: 'POST', 
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newCoffee), 
+        }
+        fetch(api,post)
+        .then((res)=>{
+            if(res.ok){
+                return res.json()
+            }else{
+                console.log(`error in fetch`)
+            }
+        })
+        .then((data)=>(setCoffee([data, ...coffee])))
+        .catch((error)=>(console.log(error)))
+        setStateDescription('');
+        setStateName('');
+    }
 
     return(
-        <h1>form</h1>
+        <>
+            <form onSubmit={FormSubmit}>
+                <input value={stateName} onChange={InputChange} type="text" placeholder="text" name="name" /><br/>
+                <input value={stateDescription} onChange={InputChange} type="text" placeholder="text" name="description" /><br/>
+                <input value='submit' type="submit" />
+            </form>
+        </>
     )
 }
 
 export default Form;
-
-
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useEffect, useState, useTransition } from "react";
 import { Outlet } from "react-router-dom";
 
 function Home(){
+    const api = 'http://localhost:3001/coffee'
+    const [coffee, setCoffee] = useState([])
+
+    useEffect(()=>{
+        fetch(api)
+        .then((res)=>{
+            if(res.ok){
+                return res.json()
+            }else {
+                console.log(`json error or something`)
+            }
+        })
+        .then((data)=>(setCoffee(data)))
+        .catch((error)=>(console.log(error)))
+        console.log(`fetch fetched`)
+    },[])
 
 
     return(
         <>
-            <h1>home component</h1>
-            <Outlet/>
+          <Outlet context={{coffee, api, setCoffee}}/>
+          <small>{
+                coffee.map((item)=>{
+                    return (
+                    <div key={item.id}>
+                        <p>{item.name}</p>
+                        <small>{item.description}</small>
+                    </div>
+                    )
+                })
+            }</small>
         </>
     )
 }
-
 
 export default Home;
